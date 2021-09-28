0155beb87069623aa1aee62b9ff09ac021ec06ca
Skorzi <lobarev_dani@mail.ru>
2021-09-28 17:47:34 +0500

    generic views

--- djangoGuide/views.py
+++ djangoGuide/views.py
@@ -1,25 +1,25 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = 'djangoGuide/index.html'
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, template, context)
+class IndexView(generic.ListView):
+    template_name = "djangoGuide/index.html"
+    context_object_name = "latest_question_list"
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "djangoGuide/detail.html", {'question':question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'djangoGuide/detail.html'
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'djangoGuide/results.html', {'question':question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'djangoGuide/detail.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
